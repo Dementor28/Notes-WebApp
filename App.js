a2346b2e9c615f99de97ef3538b206a5cc0cbf69
Abdullah <abdullah1@myseneca.ca>
2023-07-04 22:00:34 -0400

    Updates

--- App.js
+++ App.js
@@ -1,120 +1,139 @@
 const addBtn = document.querySelector("#addBtn");
 const main = document.querySelector("#main");
+let noteCounter = [];
 
-addBtn.addEventListener("click", function() {
-    addNote();
+addBtn.addEventListener("click", function () {
+  addNote();
+//   noteCounter++;
 });
 
+
 const saveNotes = () => {
-    const notes = document.querySelectorAll(".note textarea");
-    const data = [];
-    notes.forEach((note) => {
-        data.push(note.value)
-    }
-    )
-    // console.log(data);
+  const notes = document.querySelectorAll(".note textarea");
+  const data = [];
+  notes.forEach((note) => {
+    data.push(note.value);
+  });
+  if (data.length === 0) {
+    localStorage.removeItem("notes");
+    // noteCounter = [];
+  } else {
+    localStorage.setItem("notes", JSON.stringify(data));
+  }
+//   localStorage.setItem("noteCounter", JSON.stringify(noteCounter));
+};
 
-    if(data.length === 0) {
-        localStorage.removeItem("notes");
-    }else{
-        localStorage.setItem("notes", JSON.stringify(data));
+const addNote = (text = "") => {
+  const note = document.createElement("div");
+  note.classList.add("note");
 
-    }
-}
+  const storedCounter = localStorage.getItem("noteCounter");
+  if (storedCounter) {
+    noteCounter = JSON.parse(storedCounter);
+  }
 
-const addNote = (text = '') => {
-    const note = document.createElement("div");
-    note.classList.add("note");
-    note.innerHTML = `
+  const noteNumber = noteCounter.length + 1;
+  noteCounter.push(noteNumber);
+
+  note.innerHTML = `
     <div class="tool">
+        <span class="note-counter">Note No counter for now</span>
         <i class="save fa-regular fa-floppy-disk"></i>
         <i class="trash fa-solid fa-trash"></i>
     </div>
     <textarea>${text}</textarea>
-    `;
+  `;
 
-    note.querySelector(".trash").addEventListener(
-        "click",
-        function() {
-            note.remove();
-            showDeletedMessage();
-            saveNotes();
-        }
-         
-    )
-    function showDeletedMessage() {
-        const message = document.createElement("div");
-        message.textContent = "Item Deleted!";
-        message.classList.add("saved-message");
-        document.body.appendChild(message);
-        console.log(message);
-    
-        setTimeout(function() {
-            message.style.top = "0";
-        }, 100);
-    
-        setTimeout(function() {
-            message.style.top = "-50px";
-            setTimeout(function() {
-                message.remove();
-            }, 500);
-        }, 2000);
-    }
+  const textarea = note.querySelector("textarea");
+  if (text === "") {
+    textarea.value = "Add note here";
+  } else {
+    textarea.value = text;
+  }
 
-    note.querySelector(".save").addEventListener("click", function() {
-        saveNotes();
-        showSavedMessage();
-    });
-    
-    function showSavedMessage() {
-        const message = document.createElement("div");
-        message.textContent = "Item saved!";
-        message.classList.add("saved-message");
-        document.body.appendChild(message);
-        console.log(message);
-    
-        setTimeout(function() {
-            message.style.top = "0";
-        }, 100);
-    
-        setTimeout(function() {
-            message.style.top = "-50px";
-            setTimeout(function() {
-                message.remove();
-            }, 500);
-        }, 2000);
+
+  textarea.addEventListener("focus", function () {
+    if (textarea.value === "Add note here") {
+      textarea.value = "";
     }
-    
-    
-    
-    note.querySelector("textarea").addEventListener(
-        "focusout",
-        function(){
-            saveNotes();
-        }
-    );
-    main.appendChild(note);
+  });
+  
+  textarea.addEventListener("blur", function () {
+    if (textarea.value === "") {
+      textarea.value = "Add note here";
+    }
+  });
+  
+
+  note.querySelector(".trash").addEventListener("click", function () {
+    note.remove();
+    showDeletedMessage();
     saveNotes();
-}
+  });
 
+  
+  function showDeletedMessage() {
+    const message = document.createElement("div");
+    message.textContent = "Item Deleted!";
+    message.classList.add("saved-message");
+    document.body.appendChild(message);
+    console.log(message);
 
-(
-    function(){
-        const lsnotes = JSON.parse(localStorage.getItem("notes"));
-        // console.log(lsnotes);
-        if(lsnotes === null){
-            addNote();
-        }else{
-            lsnotes.forEach(
-                (lsnote) =>{
-                addNote(lsnote);
-            }
-            )
+    setTimeout(function () {
+      message.style.top = "0";
+    }, 100);
 
-        }
-    }
-)()
+    setTimeout(function () {
+      message.style.top = "-50px";
+      setTimeout(function () {
+        message.remove();
+      }, 500);
+    }, 2000);
+  }
 
+  note.querySelector(".save").addEventListener("click", function () {
+    saveNotes();
+    showSavedMessage();
+  });
+
+  function showSavedMessage() {
+    const message = document.createElement("div");
+    message.textContent = "Item saved!";
+    message.classList.add("saved-message");
+    document.body.appendChild(message);
+    console.log(message);
+
+    setTimeout(function () {
+      message.style.top = "0";
+    }, 100);
+
+    setTimeout(function () {
+      message.style.top = "-50px";
+      setTimeout(function () {
+        message.remove();
+      }, 500);
+    }, 2000);
+  }
+
+  note.querySelector("textarea").addEventListener("focusout", function () {
+    saveNotes();
+  });
+
+  main.appendChild(note);
+  saveNotes();
+};
+
+(function () {
+  const lsnotes = JSON.parse(localStorage.getItem("notes"));
+  // console.log(lsnotes);
+  if (lsnotes === null) {
+    addNote();
+  } else {
+    lsnotes.forEach((lsnote) => {
+      addNote(lsnote);
+    });
+  }
+})();
 
 const navbarToggler = document.querySelector(".navbar-toggler");
 const navbarCollapse = document.querySelector(".navbar-collapse");
@@ -123,6 +142,3 @@ const mainContent = document.querySelector("#main");
 navbarToggler.addEventListener("click", function () {
   mainContent.classList.toggle("expanded");
 });
-
-
-
